20d7cd66f92bba0ad795118a1fb8a538e7386a76
Deluan Quintao hg@deluan.com.br <none@none>
2010-10-10 22:48:41 -0300

    removed template files

--- src/main/webapp/WEB-INF/routes.groovy
+++ src/main/webapp/WEB-INF/routes.groovy
@@ -4,8 +4,3 @@ get "/blog/@year/@month/@day/@title", forward: "/WEB-INF/groovy/blog.groovy?year
 get "/something", redirect: "/blog/2008/10/20/something", cache: 2.hours
 get "/book/isbn/@isbn", forward: "/WEB-INF/groovy/book.groovy?isbn=@isbn", validate: { isbn ==~ /\d{9}(\d|X)/ }
 */
-
-// routes for the blobstore service example
-get "/upload",  forward: "/upload.gtpl"
-get "/success", forward: "/success.gtpl"
-get "/failure", forward: "/failure.gtpl"
